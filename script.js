5da2cbeec88a9f0fc9b047957eca1f55b27a5fbe
Shashank <shashanksalian96@gmail.com>
2020-02-23 21:59:15 +0530

    Using class and continuous box id

--- script.js
+++ script.js
@@ -2,120 +2,101 @@ const container = document.querySelector(".container"),
 autoBtn = document.querySelector("#auto"),
 allBox = document.querySelectorAll(".boxes");
 
-var hoveredDiv, counter = 1, selectedDivs = [], divFound = false;
-
-enterMouse = () => {
-    hoveredDiv.innerHTML = counter;
-}
-
-leaveMouse = () => {
-    hoveredDiv.innerHTML = "";
-}
-
-clickDiv = ()=>{
-    selectedDivs.push(hoveredDiv.id);
-    hoveredDiv.removeEventListener("mouseenter", enterMouse, true);
-    hoveredDiv.removeEventListener("mouseleave", leaveMouse, true);
-    hoveredDiv.removeEventListener("click", clickDiv, true);
-    hoveredDiv.style.color = "#000";
-    hoveredDiv.style.cursor = "default";
-    counter++;
-    if(counter === 26){this.clickDiv(eve, hoveredDiv)
-        counter = 1;
-        container.removeEventListener("mouseenter", parentEvent, true);
-        selectedDivs = [];
-        startGame();
+class Bingo {
+    constructor(eve){
+        this.eve = eve;
+        this.divFound = false;
+        this.counter = 1;
+        this.selectedDivs = [];
     }
-}
 
-function addEventsToDivs(){
-    hoveredDiv.addEventListener("mouseenter", enterMouse, true);
-    hoveredDiv.addEventListener("mouseleave", leaveMouse, true);
-    hoveredDiv.addEventListener("click", clickDiv, true);
-}
+    enterMouse(e){
+        if(!this.eve){
+            this.hoveredDiv = e.target;
+            // console.log(this.counter);
+            this.hoveredDiv.innerText = this.counter;
+        }else{
+            
+        }
+    }
 
-parentEvent = function(){
-    if(event.target.classList.value !== "container" && event.target.classList.value !== "heading" && event.target.classList[1] !== "gname"){
-        hoveredDiv = event.target;
-        divFound = false;
-        let i = 0;
-        do{
-            if(hoveredDiv.id === selectedDivs[i]){
-                divFound = true;
-                break;
+    leaveMouse(e){
+        if(!this.eve){
+            this.hoveredDiv = e.target;
+            this.hoveredDiv.innerText = "";
+        }else{
+            while(hoveredDiv.firstChild) { hoveredDiv.removeChild(hoveredDiv.firstChild); }
+        }
+    }
+
+    clickDiv(e){
+        if(!this.eve){
+            this.hoveredDiv = e.target;
+            this.selectedDivs.push(this.hoveredDiv.id);
+            this.hoveredDiv.removeEventListener("mouseenter", this.enterMouse, true);
+            this.hoveredDiv.removeEventListener("mouseleave", this.leaveMouse, true);
+            this.hoveredDiv.removeEventListener("click", this.clickDiv, true);
+            this.hoveredDiv.style.color = "#000";
+            this.hoveredDiv.style.cursor = "default";
+            this.counter++;
+        }else{
+            this.enterMouse(1, hoveredDiv);
+            this.newDiv.style.opacity = "1";
+            this.newDiv2.style.opacity = "1";
+            this.hoveredDiv.removeEventListener("mouseenter", this.enterMouse, true);
+        }
+    }
+
+    addEventsDivs(){
+        this.hoveredDiv.addEventListener("mouseenter", this.enterMouse, true);
+        this.hoveredDiv.addEventListener("mouseleave", this.leaveMouse, true);
+        this.hoveredDiv.addEventListener("click", this.clickDiv, true);
+    }
+
+    parentEvent = (e)=>{
+        this.hoveredDiv = e.target;
+        if(this.hoveredDiv.classList[0] === "boxes"){
+            console.log(this.hoveredDiv);
+            let i = 0;
+            do{
+                if(this.hoveredDiv.id === this.selectedDivs[i]){
+                    console.log(this.selectedDivs);
+                    this.divFound = true;
+                    break;
+                }
+                i++;
+            }while(i < this.selectedDivs.length)
+
+            if(!this.divFound){
+                this.addEventsDivs();
             }
-            i++;
-        }while(i < selectedDivs.length)
-        if(!divFound){
-            addEventsToDivs();
         }
     }
 }
 
-container.addEventListener("mouseenter", parentEvent, true);
 
+const FirstHalf = new Bingo(0);
 
 autoBtn.addEventListener("click", ()=>{
     let randNum, n = 25, arr = [];
-    for(let i = 1; i <= 25; i++){ arr[i - 1] = i }
+    for(let i = 1; i <= 25; i++){ arr[i - 1] = i; }
 
-    for(let i = 0; i < 5; i++){
-        for(let j = 0; j < 5; j++){
-            randNum = Math.floor(Math.random() * n );
+    for(let i = 1; i <= 25; i++){
+        randNum = Math.floor(Math.random() * n );
             // console.log(randNum);
-            let aDivId = "box" + i + "" + j;
+            let aDivId = "box" + i;
             let ele = document.getElementById(aDivId);
             ele.innerText = arr[randNum];
             arr.splice(randNum, 1);
             n--;
-            ele.removeEventListener("mouseenter", enterMouse, true);
-            ele.removeEventListener("mouseleave", leaveMouse, true);
-            ele.removeEventListener("click", clickDiv, true);
+            ele.removeEventListener("mouseenter", FirstHalf.enterMouse, true);
+            ele.removeEventListener("mouseleave", FirstHalf.leaveMouse, true);
+            ele.removeEventListener("click", FirstHalf.clickDiv, true);
             ele.style.color = "#000";
             ele.style.cursor = "default";
-        }
     }
 
-    container.removeEventListener("mouseenter", parentEvent, true);
-
-    autoBtn.innerHTML = "Re-Generate";
-    startGame();
+    container.removeEventListener("mouseenter", FirstHalf.parentEvent, true);
 });
 
-// function getComputerNumber(){
-//     var a;
-//     a = Math.floor(Math.random() * 25 + 1);
-//     // console.log(selectedDivs);
-//     selectedDivs.forEach(num => {
-//         if(a === num){
-//             getComputerNumber();
-//         }else{
-//             return a;
-//         }
-//     });
-// }
-
-//Second part
-
-function startGame(){
-    allBox.classList += "pointer";
-    var i,j,boxId, useNumbers = [[],[],[],[],[]];
-    for(i = 0; i < 5; i++){
-        for(j = 0; j < 5; j++){
-            boxId = "box" + i + "" + j;
-            // console.log(boxId);
-            // console.log(document.getElementById(boxId));
-            useNumbers[i][j] = document.getElementById(boxId).innerHTML;
-            // console.log(useNumbers[i][j]);
-        }
-    }
-
-    gameEvent = function(){
-        if(event.target.classList.value !== "container" && event.target.classList.value !== "heading" && event.target.classList[1] !== "gname"){
-
-        }
-    }
-    
-    container.addEventListener("click", gameEvent, true);
-
-}
+container.addEventListener("mouseenter", FirstHalf.parentEvent, true);
