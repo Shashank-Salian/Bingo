69d1e346a9e70598d75e14ca3847070e4398ea08
Shashank <shashanksalian96@gmail.com>
2020-02-24 17:50:20 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const container = document.querySelector(".container"),
 autoBtn = document.querySelector("#auto"),
-allBox = document.querySelectorAll(".boxes");
+allBox = document.querySelectorAll(".boxes"),
+rstBtn = document.querySelector("#reset");
+
 
 class Bingo {
     constructor(eve){
@@ -12,23 +14,36 @@ class Bingo {
 
     enterMouse = (e) =>{
         if(!this.eve){
+            this.hoveredDiv.style.cursor = "pointer";
             this.hoveredDiv = e.target;
             this.hoveredDiv.innerText = this.counter;
         }else{
-            
+            console.log(this.hoveredDiv.childElementCount);
+            if(this.hoveredDiv.childElementCount === 0){
+                // console.log( toString(allBox[0].childNodes) );
+                this.hoveredDiv.style.cursor = "pointer";
+                this.childOne = document.createElement("div");
+                this.childTwo = document.createElement("div");
+                this.hoveredDiv.appendChild(this.childOne);
+                this.hoveredDiv.appendChild(this.childTwo);
+                this.childOne.classList += "selected";
+                this.childTwo.classList += "selected2";
+            }
         }
     }
 
-    leaveMouse = (e) =>{
+    leaveMouse = (e) => {
         if(!this.eve){
             this.hoveredDiv = e.target;
             this.hoveredDiv.innerText = "";
         }else{
-            while(hoveredDiv.firstChild) { hoveredDiv.removeChild(hoveredDiv.firstChild); }
+            while(this.hoveredDiv.firstChild) {
+                this.hoveredDiv.removeChild(this.hoveredDiv.firstChild); 
+            }
         }
     }
 
-    clickDiv = (e) =>{
+    clickDiv = (e) => {
         if(!this.eve){
             this.hoveredDiv = e.target;
             this.selectedDivs.push(this.hoveredDiv.id);
@@ -39,13 +54,12 @@ class Bingo {
             this.hoveredDiv.style.cursor = "default";
             this.counter++;
             if(this.counter === 26){
-                container.removeEventListener("mouseenter", FirstHalf.parentEvent, true);
+                container.removeEventListener("mouseenter", firstHalf.parentEvent, true);
                 autoBtn.disabled = true;
             }
         }else{
-            this.enterMouse(1, hoveredDiv);
-            this.newDiv.style.opacity = "1";
-            this.newDiv2.style.opacity = "1";
+            this.childOne.style.opacity = "1";
+            this.childTwo.style.opacity = "1";
             this.hoveredDiv.removeEventListener("mouseenter", this.enterMouse, true);
         }
     }
@@ -54,9 +68,10 @@ class Bingo {
         this.hoveredDiv.addEventListener("mouseenter", this.enterMouse, true);
         this.hoveredDiv.addEventListener("mouseleave", this.leaveMouse, true);
         this.hoveredDiv.addEventListener("click", this.clickDiv, true);
+        // console.log(this.hoveredDiv);
     }
 
-    parentEvent = (e)=>{
+    parentEvent = (e) => {
         this.hoveredDiv = e.target;
         if(this.hoveredDiv.classList[0] === "boxes"){
             let i = 0;
@@ -77,26 +92,42 @@ class Bingo {
 }
 
 
-const FirstHalf = new Bingo(0);
+const firstHalf = new Bingo(0);
+
+
+container.addEventListener("mouseenter", firstHalf.parentEvent, true);
+
+
+let startGame = () => {
+    const secondHalf = new Bingo(1);
+    container.addEventListener("mouseenter", secondHalf.parentEvent, true);
+}
 
 autoBtn.addEventListener("click", ()=>{
     let randNum, n = 25, arr = [];
     for(let i = 1; i <= 25; i++){ arr[i - 1] = i; }
 
-    for(let i = 1; i <= 25; i++){
+    allBox.forEach(box => {
         randNum = Math.floor(Math.random() * n );
-            let aDivId = "box" + i;
-            let ele = document.getElementById(aDivId);
-            ele.innerText = arr[randNum];
-            arr.splice(randNum, 1);
-            n--;
-            ele.removeEventListener("mouseenter", FirstHalf.enterMouse, true);
-            ele.removeEventListener("mouseleave", FirstHalf.leaveMouse, true);
-            ele.removeEventListener("click", FirstHalf.clickDiv, true);
-            ele.style.color = "#000";
-            ele.style.cursor = "default";
-    }
-    container.removeEventListener("mouseenter", FirstHalf.parentEvent, true);
+        box.innerText = arr[randNum];
+        arr.splice(randNum, 1);
+        n--;
+        box.removeEventListener("mouseenter", firstHalf.enterMouse, true);
+        box.removeEventListener("mouseleave", firstHalf.leaveMouse, true);
+        box.removeEventListener("click", firstHalf.clickDiv, true);
+        box.style.color = "#000";
+        box.style.cursor = "default";
+    });
+    container.removeEventListener("mouseenter", firstHalf.parentEvent, true);
+    startGame();
 });
 
-container.addEventListener("mouseenter", FirstHalf.parentEvent, true);
+
+rstBtn.addEventListener("click", () => {
+    allBox.forEach(box => {
+        box.innerText = "";
+        firstHalf.counter = 1;
+        firstHalf.selectedDivs = [];
+        container.addEventListener("mouseenter", firstHalf.parentEvent, true);
+    });
+});
