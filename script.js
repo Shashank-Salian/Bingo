29a413df9d8024db68cd09b2a08c87e8ee34cc94
Shashank <shashanksalian96@gmail.com>
2020-03-24 21:54:26 +0530

    Computer Game and line completion check

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
-const container = document.querySelector(".container"),
-  autoBtn = document.querySelector("#auto"),
+window.onload = () => {
+
+const autoBtn = document.querySelector("#auto"),
   allBox = document.querySelectorAll(".boxes"),
   rstBtn = document.querySelector("#reset"),
-  replay = document.querySelector("#replay");
+  replay = document.querySelector("#replay"),
+  gNames = document.querySelectorAll(".gname");
+  // console.log(gNames)
 
 let isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent), counter = 1, hoveredDiv;
 
@@ -42,18 +45,8 @@ rstBtn.addEventListener("click", () => {
 });
 
 replay.addEventListener("click", () => {
-  allBox.forEach(box => {
-    box.innerText = "";
-    box.removeEventListener("mouseenter", addCross, true);
-    box.removeEventListener("mouseleave", remCross, true);
-    box.removeEventListener("click", fixCross, true);
-  });
-  counter = 1;
-  autoBtn.disabled = false;
-  rstBtn.disabled = false;
-  count = 0;
-  addEvent();
-})
+  location.reload();
+});
 
 addNum = e => {
   hoveredDiv = e.target;
@@ -113,128 +106,196 @@ fixCross = e => {
   }
   hoveredDiv.children[0].classList = "fix-cross";
   hoveredDiv.children[1].classList = "fix-cross2";
+  hoveredDiv.style.cursor = "default";
+
   if(!isMobile){
     hoveredDiv.removeEventListener("mouseenter", addCross, true);
     hoveredDiv.removeEventListener("mouseleave", remCross, true);
   }
   hoveredDiv.removeEventListener("click", fixCross, true);
-  compGame();
+  compGame(parseInt(hoveredDiv.id[3] + hoveredDiv.id[4]));
+  checkLineHor();
 }
 
-let count = 0;
+let arr = [];
 
-compGame = () => {
-  let num = Math.floor(Math.random() * 25);
-  let compDiv = document.getElementById("box" + num);
-  if(compDiv.children[0].classList[0] !== undefined  && count < 12) {
-    compGame();
-  } else {
-    count++;
-    compDiv.children[0].classList = "fix-cross";
-    compDiv.children[1].classList = "fix-cross2";
-  if(!isMobile) {
-    compDiv.removeEventListener("mouseenter", addCross, true);
-    compDiv.removeEventListener("mouseleave", remCross, true);
-  }
-  compDiv.removeEventListener("click", fixCross, true);
+for(let i = 0; i < 25; i++) arr[i - 1] = i;
+
+compGame = uid => {
+  if(arr.length > 1){
+    for(let i = 0; i < arr.length; i++)
+      if (arr[i] === uid) {
+        arr.splice(i, 1);
+        break;
+      }
+    let num = Math.floor(Math.random() * arr.length);
+    let div = document.getElementById("box" + arr[num]);
+    arr.splice(num, 1);
+    div.children[0].classList += "fix-cross comp-cross";
+    div.children[1].classList += "fix-cross2 comp-cross";
+    div.style.cursor = "default";
+    if(!isMobile){
+      div.removeEventListener("mouseenter", addCross, true);
+      div.removeEventListener("mouseleave", remCross, true);
+    }
+    div.removeEventListener("click", fixCross, true);
   }
   checkLineHor();
-  // checkLineVer();
+  checkLineVer();
 }
 
-switchLineHor = n => {
-  let i;
-  switch(n) {
-    case 5:
-      i = 5;
-      break;
-    case 10:
-      i = 10;
-      break;
-    case 15:
-      i = 15;
-      break;
-    case 20:
-      i = 20;
-      break;
-    default:
-      i = 25;
-      break;
-  }
-  return i;
-}
+let frstLine = false, secLine = false, trdLine = false, frthLine, fifthLine = false;
 
-let lineComplete = [];
 checkLineHor = () => {
-  let i = 0, id, div, n = 5;
-  do {
-    id = "box" + i;
-    div = document.getElementById(id);
-    // console.log(div);
-    if(div.children[0].classList[0] === "fix-cross"){
-      i++;
-      if(i === 4 || i === 9 || i === 14 || i === 19 || i === 24){
-        if(!lineComplete.includes(i)){
-        console.log("One line completed");
-        i = switchLineHor(n);
-        if(n <= 20)  n += 5;
-        switch(i) {
-          case 5:
-            lineComplete.push(4);
-            break;
-          case 10:
-            lineComplete.push(9);
-            break;
-          case 15:
-            lineComplete.push(14);
-            break;
-          case 20:
-            lineComplete.push(19);
-            break;
-          default:
-            lineComplete.push(24);
-        }
-      }
-      }
+  let i = 0, t = false;
+  while(i < 25) {
+    t = false;
+    if(allBox[i].firstElementChild.classList[0] === undefined) {
+      if(i < 5)  i = 5;
+      else if(i < 10)  i = 10;
+      else if(i < 15)  i = 15;
+      else if(i < 20)  i = 20;
+      else if(i < 25)  i = 25;
+      // console.log(i);
     } else {
-      i = switchLineHor(n);
-      if(n <= 20)  n += 5;
+      if(frstLine && i === 0) {
+        i = 5;
+        t = true;
+      }
+      if(secLine && i === 5) {
+        i = 10;
+        t = true;
+      }
+      if(trdLine && i === 10) {
+        i = 15;
+        t = true;
+      }
+      if(frthLine && i === 15) {
+        i = 20;
+        t = true;
+      }
+      if(fifthLine && i === 20) {
+        i = 25;
+        t = true;
+      }
+      if(i === 4){
+        console.log("First Line");
+        frstLine = true;
+        i++;
+        gnameFixCross();
+      } else if(i === 9) {
+        console.log("Second Line");
+        secLine = true;
+        i++;
+        gnameFixCross();
+      } else if(i === 14) {
+        console.log("Third Line");
+        trdLine = true;
+        i++;
+        gnameFixCross();
+      } else if(i === 19) {
+        console.log("Fourth Line");
+        frthLine = true;
+        i++;
+        gnameFixCross();
+      } else if(i === 24) {
+        console.log("Fifth line");
+        fifthLine = true;
+        i++;
+        gnameFixCross();
+      } else if(!t) {
+        i++;
+      }
     }
-  } while(i < n)
-  // console.log(div);
-  console.log(lineComplete);
+  }
 }
+
+let verArr = []
+
+changePosVer = () => {
+  let n = 0;
+  for(let i = 0; i < allBox.length; i++) {
+    verArr[Math.floor(i/5+n)] = allBox[i];
+    n += 5;
+    if(n === 25)  n = 0;
+  }
+}
+
+let frstRow = false, secRow = false, trdRow = false, frthRow = false, fifthRow = false;
 
 checkLineVer = () => {
-  let i = 0, id, div, n = 20;
-  do {
-    id = "box" + i;
-    div = document.getElementById(id);
-    if(div.children[0].classList[0] === "fix-cross")  i += 5;
-    else{
-      switch(n) {
-        case 20:
-          n = 9;
-          i = 5;
-          break;
-        case 9:
-          n = 14;
-          i = 10;
-          break;
-        case 14:
-          n = 19;
-          i = 15;
-          break;
-        case 19:
-          n = 24;
-          i = 20;
-          break;
-        default:
-          i = 25;
-          break;
+  let i = 0, t = false;
+  while(i < 25) {
+    t = false;
+    if(verArr[i].firstElementChild.classList[0] === undefined) {
+      if(i < 5)  i = 5;
+      else if(i < 10)  i = 10;
+      else if(i < 15)  i = 15;
+      else if(i < 20)  i = 20;
+      else if(i < 25)  i = 25;
+      // console.log(i);
+    } else {
+      if(frstRow && i === 0) {
+        i = 5;
+        t = true;
+      }
+      if(secRow && i === 5) {
+        i = 10;
+        t = true;
+      }
+      if(trdRow && i === 10) {
+        i = 15;
+        t = true;
+      }
+      if(frthRow && i === 15) {
+        i = 20;
+        t = true;
+      }
+      if(fifthRow && i === 20) {
+        i = 25;
+        t = true;
+      }
+      if(i === 4){
+        console.log("First Row");
+        frstRow = true;
+        i++;
+        gnameFixCross();
+      } else if(i === 9) {
+        console.log("Second Row");
+        secRow = true;
+        i++;
+        gnameFixCross();
+      } else if(i === 14) {
+        console.log("Third Row");
+        trdRow = true;
+        i++;
+        gnameFixCross();
+      } else if(i === 19) {
+        console.log("Fourth Row");
+        frthRow = true;
+        i++;
+        gnameFixCross();
+      } else if(i === 24) {
+        console.log("Fifth Row");
+        fifthRow = true;
+        i++;
+        gnameFixCross();
+      } else if(!t) {
+        i++;
       }
     }
-  } while(i < n)
+  }
+}
+
+gnameFixCross = () => {
+  let j;
+  for(j = 0; j < gNames.length; j++) {
+    if(gNames[j].firstElementChild.classList[0] === undefined) {
+      gNames[j].children[0].classList = "game-cross";
+      gNames[j].children[1].classList = "game-cross2";
+      break;
+    } else if(j === 4)  console.log("Game Over");
+  }
 }
 
 addEvent = () => {
@@ -270,3 +331,6 @@ startGame = () => {
 
 
 addEvent();
+changePosVer();
+
+}
